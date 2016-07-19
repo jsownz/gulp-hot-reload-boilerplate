6f1e220ee35048b43cdb44b02f4a4376f38cf3d1
Jason Sohl <jason@frozensoliddesigns.com>
2016-07-19 16:20:00 -0700

    ejs template

--- src/server.js
+++ src/server.js
@@ -4,13 +4,18 @@ var api = require('./api')
 
 var app = express()
 
+app.set('view engine', 'ejs');
+
 app.get('/test', function(req, res) {
   res.send('Hello, Hot Reload!')
 })
 
 app.get('/api', api)
 
-app.use('/', express.static(path.join(__dirname, 'static')))
+// app.use('/', express.static(path.join(__dirname, 'static')))
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
 
 var server = app.listen(1337, '127.0.0.1', function () {
   var port = server.address().port
